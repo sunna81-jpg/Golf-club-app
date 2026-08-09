d7316ae798144009ab9ce6819cbdbe7d39207859
sunna81-jpg <sunna81@gmail.com>
2026-08-09 16:13:46 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'p1-golf-society-v1';
+const APP_SHELL = ['./', './index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request).then((res) => res || caches.match('./index.html'))
+    )
+  );
+});
